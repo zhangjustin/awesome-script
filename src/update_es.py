dd4b97f1240913d6b2b8eead3e6fe596d99cb827
zhangjustin <hizcyu@126.com>
2018-04-09 18:27:22 +0800

    construct

---
+++ src/update_es.py
@@ -0,0 +1,15 @@
+# -*- coding:utf8 -*-
+
+class data_to_es(object):
+    def __init__(self, data):
+        self.data = data
+        
+    def update_es(self):
+        pass
+
+    def index_new_data(self):
+        pass
+
+    def format_data(self):
+        pass
+
