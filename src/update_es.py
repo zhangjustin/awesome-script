2b00a7bf44790e40a8ad2185fc0b8a1d0fe42555
zhangjustin <hizcyu@126.com>
2018-04-10 08:10:23 +0800

    construct

--- src/update_es.py
+++ src/update_es.py
@@ -3,8 +3,22 @@
 class data_to_es(object):
     def __init__(self, data):
         self.data = data
-        
+
     def update_es(self):
+        
+        {
+            '_op_type': 'delete',
+            '_index': 'index-name',
+            '_type': 'document',
+            '_id': 42,
+            };
+        {
+        '_op_type': 'update',
+        '_index': 'index-name',
+        '_type': 'document',
+        '_id': 42,
+        'doc': {'question': 'The life, universe and everything.'}
+        };
         pass
 
     def index_new_data(self):
